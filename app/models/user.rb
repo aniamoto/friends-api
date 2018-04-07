bab3b88a95f99570062d89241fb34835c9c94d78
aniamoto <anna.kusyk@g.pl>
2018-04-07 14:49:33 +0800

    Add User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User
   include Neo4j::ActiveNode
   property :email, type: String, constraint: :unique
 
+  has_many :both, :friends, type: 'FRIENDS_WITH', model_class: :User, unique: true
+  has_many :in, :subscribers, type: 'FOLLOWS', model_class: :User, unique: true
+  has_many :out, :subscriptions, type: 'FOLLOWS', model_class: :User, unique: true
+
   validates :email, presence: true, uniqueness: true
 end
